b9e1914502781707088a1022d87dd07837227754
Alex White <14aaw4@queensu.ca>
2019-09-07 17:07:49 -0400

    Updated visulization and added 'picture points' which are shown on both maps.

--- orbit_simulaiton.py
+++ orbit_simulaiton.py
@@ -12,29 +12,6 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QLi
 import matplotlib.animation as animation
 from PyQt5.QtCore import pyqtSlot
 
-def createSphere(fig, image_file):
-
-    # load and map the texture
-    img = tvtk.JPEGReader()
-    img.file_name = image_file
-    texture = tvtk.Texture(input_connection=img.output_port, interpolate=1)
-    # (interpolate for a less raster appearance when zoomed in)
-
-    # use a TexturedSphereSource, a.k.a. getting our hands dirty
-    R = 6371*1000 
-    Nrad = 360 
-
-    # create the sphere source with a given radius and angular resolution
-    sphere = tvtk.TexturedSphereSource(radius=R, theta_resolution=Nrad,
-                                       phi_resolution=Nrad)
-
-    # assemble rest of the pipeline, assign texture    
-    sphere_mapper = tvtk.PolyDataMapper(input_connection=sphere.output_port)
-    sphere_actor = tvtk.Actor(mapper=sphere_mapper, texture=texture)
-    fig.scene.add_actor(sphere_actor)
-    sphere_actor.rotate_z(180)
-    return fig, sphere_actor
-##end createSphere##
 
 class Sat:
     pos = np.zeros(3)
@@ -147,7 +124,7 @@ def updateGravity(_sat):
 
 def stepTime(sat, total_time, dt):
     steps = int(total_time/dt)
-    #this function is made to take multiple steps in one call but it's current use only steps once
+    #this function is made to take multiple steps in one call 
     time_array = np.linspace(0,total_time,steps)
     for t in range(len(time_array)):
         sat = spinEarth(sat, dt)
@@ -170,11 +147,12 @@ def setupGroundMap():
 	img = plt.imread('blue_marble_5400x2700.jpg')
 	plt.imshow(img, extent=[-180,180,-90,90])
 	track, = plt.plot([], [], 'y.', markersize=0.5)
-	sat_marker, = plt.plot(0,0, 'r.', markersize=5)
-	return track, sat_marker 
+	sat_marker, = plt.plot(0,0, 'r.', markersize=7)
+	picture_marker, = plt.plot([], [], 'm.', markersize=4)
+	return track, sat_marker, picture_marker 
 
-def updateGroundMap(_ground_track, _sat_marker, _sat):
-	#convert x,y,z to long and lat
+def updateGroundMap(_ground_track, _sat_marker, _sat, _picture_marker, _picture_points):
+	#convert sat's x,y,z to long and lat
 	R = np.sqrt(_sat.last_pos[:,0]**2 + _sat.last_pos[:,1]**2 + _sat.last_pos[:,2]**2) 
 	theta = np.arctan2(_sat.last_pos[:,0],_sat.last_pos[:,1]) #long
 	phi = np.arcsin(_sat.last_pos[:,2]/R)  #lat
@@ -182,13 +160,47 @@ def updateGroundMap(_ground_track, _sat_marker, _sat):
 	phi = np.degrees(phi)
 	#now update and plot
 	_ground_track.set_data(theta,phi)
+
 	#find the long and lat point that's in the centre of all the data (this is where the sat is)
 	sat_phi = phi[int(_sat.period()/10)]
 	sat_theta = theta[int(_sat.period()/10)]
 	#now update and plot th sat's pos
 	_sat_marker.set_data(sat_theta,sat_phi)
+	
+	#convert the picture points from x,y,x to long and lat
+	R = np.sqrt(_picture_points[:,0]**2 + _picture_points[:,1]**2 + _picture_points[:,2]**2) 
+	theta = np.arctan2(_picture_points[:,0],_picture_points[:,1]) #long
+	phi = np.arcsin(_picture_points[:,2]/R)  #lat
+	theta = np.degrees(theta)
+	phi = np.degrees(phi)
+	#now update and plot
+	_picture_marker.set_data(theta,phi)
 
-def simulate(sim_time, dt, sat, ground_track, sat_marker):
+def createSphere(fig, image_file):
+
+    # load and map the texture
+    img = tvtk.JPEGReader()
+    img.file_name = image_file
+    texture = tvtk.Texture(input_connection=img.output_port, interpolate=1)
+    # (interpolate for a less raster appearance when zoomed in)
+
+    # use a TexturedSphereSource, a.k.a. getting our hands dirty
+    R = 6371*1000 
+    Nrad = 360 
+
+    # create the sphere source with a given radius and angular resolution
+    sphere = tvtk.TexturedSphereSource(radius=R, theta_resolution=Nrad,
+                                       phi_resolution=Nrad)
+
+    # assemble rest of the pipeline, assign texture    
+    sphere_mapper = tvtk.PolyDataMapper(input_connection=sphere.output_port)
+    sphere_actor = tvtk.Actor(mapper=sphere_mapper, texture=texture)
+    fig.scene.add_actor(sphere_actor)
+    sphere_actor.rotate_z(180)
+    return fig, sphere_actor
+##end createSphere##
+
+def simulate(sim_time, dt, sat, ground_track, sat_marker, picture_marker, picture_points):
 	elapsed_time = 0 
 	start_time = time.time()
 	step_start_time = start_time 
@@ -206,7 +218,7 @@ def simulate(sim_time, dt, sat, ground_track, sat_marker):
 		sat = stepTime(sat, dt, dt)
 	
 		## create 2d map with track 
-		updateGroundMap(ground_track, sat_marker, sat)		
+		updateGroundMap(ground_track, sat_marker, sat, picture_marker, picture_points)		
 		plt.pause(0.05)		
 		plt.draw()
 	
@@ -214,7 +226,7 @@ def simulate(sim_time, dt, sat, ground_track, sat_marker):
 		mlab.clf()
 		scaling_factor = m.sqrt(400000**2/3)	
 		line = mlab.quiver3d(sat.last_pos[int(sat.period()/dt),1],sat.last_pos[int(sat.period()/dt),0],sat.last_pos[int(sat.period()/dt),2],sat.spin_vector[0],sat.spin_vector[1],sat.spin_vector[2], scale_factor=400000, line_width=2, figure=fig, color=(1,0,0), mode='2darrow')
-		mlab.points3d(6371*1000, 0, 0, figure = fig, scale_factor = 200000, color=(1,0,1))		
+		mlab.points3d(picture_points[:,0], picture_points[:,1], picture_points[:,2], figure = fig, scale_factor = 200000, color=(1,0,1))		
 		mlab.draw()
 		#make sure step only last one second in real time
 		if abs(time.time()-step_start_time) <= 1:
@@ -230,8 +242,9 @@ def simulate(sim_time, dt, sat, ground_track, sat_marker):
 	
 if __name__ == "__main__":
 	##SETUP##
+	picture_points = np.array([[6371*1000, 0, 0],[0,6371*1000,0]])
 	sat = Sat(np.array([0,(6771*1000),0]).astype(float), np.array([5000,0,5000]).astype(float), np.array([0,0]).astype(float))#polar orbit is 8000m/s
-	ground_track, sat_marker = setupGroundMap()
+	ground_track, sat_marker, picture_marker = setupGroundMap()
 	fig = mlab.figure(size = (600,600), bgcolor=(0,0,0))
 	image_file = 'blue_marble_5400x2700.jpg'
 	fig, sphere = createSphere(fig, image_file)
@@ -241,9 +254,9 @@ if __name__ == "__main__":
 	earth_rotation_time = 24*60*60
 
 	sat.datt[1] = 2*m.pi/(sat.period())
-	simulate(sim_time, dt, sat, ground_track, sat_marker)
+	simulate(sim_time, dt, sat, ground_track, sat_marker, picture_marker, picture_points)
 #	sat = stepTime(sat, earth_rotation_time, dt)
-	updateGroundMap(ground_track, sat_marker, sat)
+	updateGroundMap(ground_track, sat_marker, sat, picture_marker, picture_points)
 	longitude = 0
 	latitude = 0
 	plt.show()
